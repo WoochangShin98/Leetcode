4ce0c3bbe2da2cdc2715012b16fc2dfa8e7c6b82
Woochang Shin <shin0490@umn.edu>
2026-08-11 16:51:18 -0500

    Time: 1502 ms (8.12%), Space: 0B (100%) - LeetHub

---
+++ 1083-sales-analysis-ii/1083-sales-analysis-ii.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+
+SELECT DISTINCT(buyer_id)
+FROM Sales s
+JOIN Product p
+ON p.product_id = s.product_id
+WHERE product_name = 'S8'
+AND buyer_id NOT IN(
+    SELECT buyer_id
+    FROM Sales s
+    JOIN Product p
+    ON s.product_id = p.product_id
+    WHERE product_name = 'iPhone'
+)
