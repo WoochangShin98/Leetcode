be2ed11496fa147a4eab39d0cab50818816de169
Woochang Shin <shin0490@umn.edu>
2026-08-11 16:52:22 -0500

    Time: 525 ms (25.88%), Space: 0B (100%) - LeetHub

---
+++ 1173-immediate-food-delivery-i/1173-immediate-food-delivery-i.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+SELECT
+ROUND(COUNT(CASE
+WHEN order_date = customer_pref_delivery_date 
+THEN 1
+END)*100 / COUNT(*)
+,2)AS immediate_percentage
+FROM Delivery;
