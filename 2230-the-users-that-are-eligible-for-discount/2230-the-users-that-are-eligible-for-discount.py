a9feb7281ae56d24a1f67d945a05ca93d61e1dd8
Woochang Shin <shin0490@umn.edu>
2026-08-24 14:01:28 -0500

    Time: 308 ms (64.71%), Space: 68.6 MB (58.82%) - LeetHub

---
+++ 2230-the-users-that-are-eligible-for-discount/2230-the-users-that-are-eligible-for-discount.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from datetime import datetime
+
+def find_valid_users(purchases: pd.DataFrame, start_date: datetime, end_date: datetime, min_amount: int) -> pd.DataFrame:
+    
+    filtered = (
+        purchases['time_stamp'].between(start_date, end_date)
+        & (purchases['amount'] >= min_amount)
+    )
+
+    df = purchases[filtered]
+
+    return (
+        df[['user_id']]
+        .drop_duplicates()
+        .sort_values('user_id')
+    )
