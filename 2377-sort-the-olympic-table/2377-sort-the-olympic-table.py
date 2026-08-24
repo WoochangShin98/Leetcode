5c81e4ec977bce2f54372b481e271ac29ed83bef
Woochang Shin <shin0490@umn.edu>
2026-08-24 15:19:04 -0500

    Time: 339 ms (6.25%), Space: 67.8 MB (18.75%) - LeetHub

---
+++ 2377-sort-the-olympic-table/2377-sort-the-olympic-table.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def sort_table(olympic: pd.DataFrame) -> pd.DataFrame:
+    df = olympic.sort_values(['gold_medals','silver_medals','bronze_medals','country'],
+    ascending=[False,False,False,True])
+    return df
