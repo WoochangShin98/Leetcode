41a620a68b6871b66f77c5accacd4646c418e343
Woochang Shin <shin0490@umn.edu>
2026-08-24 14:59:31 -0500

    Time: 296 ms (79.17%), Space: 68.8 MB (16.67%) - LeetHub

---
+++ 2339-all-the-matches-of-the-league/2339-all-the-matches-of-the-league.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def find_all_matches(teams: pd.DataFrame) -> pd.DataFrame:
+    df = teams.merge(teams,how='cross')
+    df = df[df['team_name_x'] != df['team_name_y']]
+    df = df.rename(columns={'team_name_x':'home_team','team_name_y':'away_team'})
+    
+    return df
