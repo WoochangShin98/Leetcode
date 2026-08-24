dcda0dd0cb2b1c73c21fb71015f127bd56cfb7ac
Woochang Shin <shin0490@umn.edu>
2026-08-24 14:53:08 -0500

    Time: 978 ms (40.2%), Space: 0B (100%) - LeetHub

---
+++ 2329-product-sales-analysis-v/2329-product-sales-analysis-v.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT s.user_id,
+SUM(s.quantity * p.price) AS spending
+FROM Sales s
+LEFT JOIN Product p
+ON s.product_id = p.product_id
+GROUP BY user_id
+ORDER BY spending DESC, user_id ASC;
