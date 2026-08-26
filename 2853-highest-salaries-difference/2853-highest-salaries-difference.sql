c9c6400496866d042fe7a30024fa72bbb48fdc1f
Woochang Shin <shin0490@umn.edu>
2026-08-26 14:05:18 -0500

    Time: 302 ms (98.92%), Space: 0B (100%) - LeetHub

---
+++ 2853-highest-salaries-difference/2853-highest-salaries-difference.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT ABS(
+    MAX(CASE WHEN department = 'Engineering' THEN salary END)
+    -
+    MAX(CASE WHEN department = 'Marketing' THEN salary END)
+) AS salary_difference
+FROM Salaries;
+
+
+#SELECT MAX(salary)
+#FROM Salaries
+#WHERE department = 'Engineering'
