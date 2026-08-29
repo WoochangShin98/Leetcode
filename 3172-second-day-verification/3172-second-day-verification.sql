1e25034d4fc4f584b9bc5e606062cada7118811f
Woochang Shin <shin0490@umn.edu>
2026-08-29 00:09:41 -0500

    Time: 511 ms (90.36%), Space: 0B (100%) - LeetHub

---
+++ 3172-second-day-verification/3172-second-day-verification.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT user_id
+FROM emails e
+LEFT JOIN texts t
+ON e.email_id = t.email_id 
+WHERE t.signup_action = 'Verified'
+ AND DATE(t.action_date) = DATE(e.signup_date + INTERVAL 1 DAY)
+ORDER BY e.user_id
