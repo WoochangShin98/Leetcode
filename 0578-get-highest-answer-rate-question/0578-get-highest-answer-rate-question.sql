a3c32a60aada3a7de0514c8cdbf86ab3c071df6d
Woochang Shin <shin0490@umn.edu>
2026-09-03 14:33:37 -0500

    Time: 269 ms (81.02%), Space: 0B (100%) - LeetHub

---
+++ 0578-get-highest-answer-rate-question/0578-get-highest-answer-rate-question.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT question_id AS survey_log
+FROM SurveyLog
+GROUP BY question_id
+ORDER BY
+    SUM(action = 'answer') / SUM(action = 'show') DESC,
+    question_id ASC
+LIMIT 1;
