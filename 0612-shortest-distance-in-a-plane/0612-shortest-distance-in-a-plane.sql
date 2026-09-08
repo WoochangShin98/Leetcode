20fca47fa0ceb5db49659ad608700069bfd0a6dd
Woochang Shin <shin0490@umn.edu>
2026-09-08 16:29:10 -0500

    Time: 256 ms (59.09%), Space: 0B (100%) - LeetHub

---
+++ 0612-shortest-distance-in-a-plane/0612-shortest-distance-in-a-plane.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+SELECT ROUND(
+    MIN(
+        SQRT(
+            POW(p1.x - p2.x, 2) +
+            POW(p1.y - p2.y, 2)
+        )
+    ),
+    2
+) AS shortest
+FROM Point2D p1
+CROSS JOIN Point2D p2
+WHERE p1.x != p2.x OR p1.y != p2.y;
