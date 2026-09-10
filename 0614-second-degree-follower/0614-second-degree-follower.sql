48290747d818ddfff5be773c8481888afe0887bb
Woochang Shin <shin0490@umn.edu>
2026-09-10 00:51:59 -0500

    Time: 451 ms (65.94%), Space: 0B (100%) - LeetHub

---
+++ 0614-second-degree-follower/0614-second-degree-follower.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT followee AS follower,
+COUNT(*) AS num
+FROM Follow
+WHERE followee IN (
+    SELECT follower
+    FROM Follow
+)
+GROUP BY followee
+ORDER BY follower;
